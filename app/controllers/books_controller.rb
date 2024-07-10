c9c2a17b6852ac295a155a7b76bf340ad941fb34
EC2 Default User <ec2-user@ip-172-31-45-130.ap-northeast-1.compute.internal>
2024-07-10 15:05:23 +0000

    できてたことができなくなっている。createbook,destory..

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
     @books = Book.all
     @user = current_user
-  end 
+  end
 
   def create
     book = Book.new(book_params)
@@ -13,13 +13,13 @@ class BooksController < ApplicationController
       redirect_to books_path
     else
       render :new
-    end 
-  end 
+    end
+  end
 
   def index
     @books = Book.all
     @user = current_user
-    
+
   end
 
   def show
@@ -27,17 +27,31 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = current_user
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
   
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to 'books_path'
   end
-  
+
   private
   
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end 
-  
+   def book_params
+     params.require(:book).permit(:title, :body)
+   end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as:'about'
   
-  resources :books, onlt: [:new, :create, :index, :show, :destory]
+  resources :books, onlt: [:new, :create, :index, :show, :edit, :update, :destory]
   resources :users, only: [:new, :index, :show, :edit, :update]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
