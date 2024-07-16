90ad8d299d21e575b315044317eaf5445e1529aa
EC2 Default User <ec2-user@ip-172-31-45-130.ap-northeast-1.compute.internal>
2024-07-16 09:57:14 +0000

    メンター質問前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,19 +8,22 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+  
     if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id) 
     else
       @books = Book.all
       @user = current_user
-      render :index
+      render :index 
     end
   end
+  
 
   def index
+    @book = Book.new
     @books = Book.all
     @user = current_user
   end
@@ -59,6 +62,7 @@ class BooksController < ApplicationController
    
    def is_matching_login_user
      book = Book.find(params[:id])
+     user = book.user
      unless user.id == current_user.id
        redirect_to books_path
      end 
